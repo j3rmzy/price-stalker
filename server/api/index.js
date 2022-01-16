ad2ccaf5c97004d15cbd73c58c2c951dbe758faf
Jermaine G <jgyekye@gmail.com>
2022-01-16 13:20:36 +0000

    feat(api): added Express for api
    Added Express for API and mock data

---
+++ server/api/index.js
@@ -0,0 +1,31 @@
+const scrapersMockData = require('../../mocks/scrapers.json');
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+/**
+ * GET - /scrapers/
+ * GET - /scapers/:id
+ * POST - /scrapers/
+ * DELETE - /scrapers/:id
+ */
+
+app.get('/scrapers', (req, res) => {
+    res.send({
+        data: scrapersMockData
+    })
+});
+
+app.post('/scrapers', (req, res) => {
+    res.send('POST scraper to DB')
+})
+
+app.delete('/scrapers/:id', (req, res) => {
+    res.send('DELETE scraper')
+})
+
+app.listen(port, () => {
+    console.log(`Price Stalker API listening on port: ${port}`)
+});
--- ui/src/data/index.tsx
+++ ui/src/data/index.tsx
@@ -1,3 +1,4 @@
+import scrapersData from "../../../mocks/scrapers.json";
 
 export interface IProduct {
     productName: string,
@@ -13,23 +14,4 @@ export interface IProductScrapers {
     productPrice: number
 }
 
-export const getScrapers = () => [
-    {
-        id: "1",
-        productName: "Adidas NMD R1",
-        productImage: "https://i8.amplience.net/i/jpl/jd_464981_a?qlt=92&w=750&h=531&v=1&fmt=auto",
-        productScrapers: [
-            {
-                productWebsiteName: "JD Sports",
-                productWebsiteURL: "https://www.jdsports.co.uk/product/adidas-originals-nmdr1-og/16169166/",
-                productPrice: 110
-            }, {
-                productWebsiteName: "Adidas UK",
-                productWebsiteURL: "",
-                productPrice: 70
-            }
-        ],
-        dateCreated: Date.now(),
-        dateLastChecked: new Date("12/05/2000")
-    }
-];
+export const getScrapers = () => scrapersData;
